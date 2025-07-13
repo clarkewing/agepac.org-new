c985569bbcb32f8044360c2b93340f62af19a163
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-07-13 11:20:07 +0200

    Add developer login button

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,52 +1,66 @@
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
+    <flux:heading class="text-center" size="xl">{{ __('Welcome back') }}</flux:heading>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="text-center" :status="session('status')" />
+    <div class="space-y-4">
+        @env('local')
+            <x-login-link />
+        @endenv
+    </div>
 
-    <form wire:submit="login" class="flex flex-col gap-6">
-        <!-- Email Address -->
-        <flux:input
-            wire:model="email"
-            :label="__('Email address')"
-            type="email"
-            required
-            autofocus
-            autocomplete="email"
-            placeholder="email@example.com"
-        />
+    <flux:separator text="or" />
 
-        <!-- Password -->
-        <div class="relative">
+    <div class="flex flex-col gap-6">
+        <!-- Session Status -->
+        <x-auth-session-status class="text-center" :status="session('status')" />
+
+        <form wire:submit="login" class="flex flex-col gap-6">
+            <!-- Email -->
             <flux:input
-                wire:model="password"
-                :label="__('Password')"
-                type="password"
+                wire:model="email"
+                :label="__('Email')"
+                type="email"
                 required
-                autocomplete="current-password"
-                :placeholder="__('Password')"
-                viewable
+                autofocus
+                autocomplete="email"
+                placeholder="email@example.com"
             />
 
-            @if (Route::has('password.request'))
-                <flux:link class="absolute end-0 top-0 text-sm" :href="route('password.request')" wire:navigate>
-                    {{ __('Forgot your password?') }}
-                </flux:link>
-            @endif
-        </div>
+            <!-- Password -->
+            <flux:field>
+                <div class="mb-3 flex justify-between">
+                    <flux:label>Password</flux:label>
 
-        <!-- Remember Me -->
-        <flux:checkbox wire:model="remember" :label="__('Remember me')" />
+                    @if (Route::has('password.request'))
+                        <flux:link variant="subtle" class="text-sm" :href="route('password.request')" wire:navigate>
+                            {{ __('Forgot password?') }}
+                        </flux:link>
+                    @endif
+                </div>
 
-        <div class="flex items-center justify-end">
-            <flux:button variant="primary" type="submit" class="w-full">{{ __('Log in') }}</flux:button>
-        </div>
-    </form>
+                <flux:input
+                    wire:model="password"
+                    type="password"
+                    required
+                    autocomplete="current-password"
+                    :placeholder="__('Password')"
+                    viewable
+                    placeholder="Your password"
+                />
+            </flux:field>
 
-    @if (Route::has('register'))
-        <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            <span>{{ __('Don\'t have an account?') }}</span>
-            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
-        </div>
-    @endif
+            <!-- Remember Me -->
+            <flux:checkbox wire:model="remember" :label="__('Remember me')" />
+
+            <div class="flex items-center justify-end">
+                <flux:button variant="primary" type="submit" class="w-full">{{ __('Log in') }}</flux:button>
+            </div>
+        </form>
+
+        @if (Route::has('register'))
+            <flux:subheading class="text-center">
+                <span>{{ __('First time around here?') }}</span>
+                <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
+            </flux:subheading>
+        @endif
+    </div>
 </div>
---
+++ resources/views/vendor/login-link/loginLinkButton.blade.php
@@ -0,0 +1,7 @@
+<flux:button
+    {{ $attributes->class('w-full') }}
+    icon="code-bracket"
+    type="submit"
+>
+    {{ $label }}
+</flux:button>
