ceb572867445a392786d08f9ea31191ddd09cf67
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-08-06 23:15:39 +0200

    Setup public routes

--- routes/public.php
+++ routes/public.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::view('/', 'public.welcome')->name('home');
+
+Route::view('/privacy', 'public.terms', ['terms' => 'privacy']);
+Route::view('/terms', 'public.terms', ['terms' => 'terms']);
+
+Route::view('epl/selection', 'public.selection');
+Route::view('epl/training', 'public.training');
+
+Route::view('association', 'public.about');
+Route::view('association/team', 'public.team');
+
+Route::view('contact', 'public.contact');
+
+Route::view('press', 'public.press');
+
+Route::view('remembering', 'public.remembering');
