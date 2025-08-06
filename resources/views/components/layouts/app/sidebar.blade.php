9baa0919f9fc303856b1ac5ff2eda1fca2e36f83
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-08-06 23:48:04 +0200

    Setup link back to public site

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -24,12 +24,8 @@
             <flux:spacer />
 
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-                </flux:navlist.item>
-
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
+                <flux:navlist.item icon="globe-alt" href="{{ route('public.home') }}" target="_blank">
+                    {{ __('Public Site') }}
                 </flux:navlist.item>
             </flux:navlist>
 
